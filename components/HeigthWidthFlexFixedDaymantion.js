3c9925fc8e33a02fccb911e2a56b73d65dfbf973
Ekramul Islam Sumon <mmsumon799@gmail.com>
2021-07-24 18:06:36 +0600

    HeigthWidthFlexFixedDaymantion

---
+++ components/HeigthWidthFlexFixedDaymantion.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { View }             from "react-native";
+
+class HeigthWidthFlexFixedDaymantion extends Component {
+  render() {
+    return (
+      <View style={{ backgroundColor: "black", flex: 10 }}>
+        <View style={{ backgroundColor: "red", flex: 4 }}></View>
+        <View style={{ backgroundColor: "blue", flex: 2 }}></View>
+        <View style={{ backgroundColor: "green", flex: 3 }}></View>
+
+        <View style={{ backgroundColor: "red", height: 50, width: 100 }}></View>
+        <View style={{ backgroundColor: "blue", height: 50, width: 200 }}></View>
+        <View style={{ backgroundColor: "green", height: 50, width: 300 }}></View>
+        <View style={{ backgroundColor: "red", height: 50, width: 395 }}></View>
+        <View style={{ backgroundColor: "red", height: 50, width: 395 }}></View>
+
+      </View>
+    );
+  }
+}
+
+export default HeigthWidthFlexFixedDaymantion;
