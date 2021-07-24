aef7ce9b8f3e706a142f573df0c87e624ad4adfc
Ekramul Islam Sumon <mmsumon799@gmail.com>
2021-07-24 13:53:17 +0600

    View Compo Style

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import {Text, View}       from "react-native";
 class App extends Component {
     render() {
         return (
-            <View>
+            <View style={{flexDirection: 'row', justifyContent:"center", height: "100%", width: "100%", backgroundColor: 'red'}}>
                 <Text>Hello</Text>
+                <Text>AAA</Text>
+                <Text>SSSS</Text>
+                <Text>DDDD</Text>
             </View>
         );
     }
