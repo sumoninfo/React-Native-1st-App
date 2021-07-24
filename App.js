5a4242910b48de85eb4cb4e2f435d1749c3736d1
Ekramul Islam Sumon <mmsumon799@gmail.com>
2021-07-24 18:02:32 +0600

    fixed dp desing height width

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { Image, Text, View } from "react-native";
 class App extends Component {
   render() {
     return (
-      <View style={{ backgroundColor: "black", flex: 10 }}>
-        <View style={{ backgroundColor: "red", flex: 4 }}></View>
-        <View style={{ backgroundColor: "blue", flex: 2 }}></View>
-        <View style={{ backgroundColor: "green", flex: 3 }}></View>
+      <View style={{ backgroundColor: "white"}}>
+        <View style={{ backgroundColor: "red", height:50, width:100 }}></View>
+        <View style={{ backgroundColor: "blue", height:50, width:200 }}></View>
+        <View style={{ backgroundColor: "green", height:50, width:300 }}></View>
       </View>
     );
   }
