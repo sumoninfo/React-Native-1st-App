0fc66d56ec98f276ef1b1c09dafaaa954aaf1fae
Ekramul Islam Sumon <mmsumon799@gmail.com>
2021-07-24 14:01:40 +0600

    ViewComponentStyling

---
+++ components/ViewComponentStyling.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {Text, View}       from "react-native";
+
+class ViewComponentStyling extends Component {
+    render() {
+        return (
+            <View style={{
+                flexDirection: 'row',
+                height       : "100%",
+                width        : "100%",
+            }}>
+                <View style={{
+                    backgroundColor: "orange",
+                    flexDirection  : 'column',
+                    height         : "100%",
+                    width          : "32%"
+                }}>
+                </View>
+                <View style={{
+                    backgroundColor: "red",
+                    flexDirection  : 'column',
+                    height         : "100%",
+                    width          : "32%"
+                }}>
+                </View>
+                <View style={{
+                    backgroundColor: "gray",
+                    flexDirection  : 'column',
+                    height         : "100%",
+                    width          : "32%"
+                }}>
+                </View>
+            </View>
+        );
+    }
+}
+
+export default ViewComponentStyling;
