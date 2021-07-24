bdacad23b28355db2f810dc7a232eebd065b2fd2
shaheen2013: 20040111 <coredeveloper.2013@gmail.com>
2021-07-24 13:15:27 +0600

    class component state data show with updated on click btn

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import React, {Component} from 'react';
-import {Text, View} from "react-native";
-import Home from "./views/Home";
+import React, {Component}   from 'react';
+import {Button, Text, View} from "react-native";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'Summon',
+            age : 24,
+            city: 'Dhaka'
+        }
+    }
+
+    changeData = () => {
+        this.setState({
+            name: 'Samy',
+            age : 20,
+            city: 'Mohammadpur'
+        })
+    }
+
     render() {
         return (
             <View>
-                <Home/>
+                <Text>Name: {this.state.name}</Text>
+                <Text>Age: {this.state.age}</Text>
+                <Text>City: {this.state.city}</Text>
+                <Button title="Click me" onPress={this.changeData}/>
             </View>
         );
     }
