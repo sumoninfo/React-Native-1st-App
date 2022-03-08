e461333154900c36647e4da0f5da2a0bdb26145d
Ekramul Islam Sumon <mmsumon799@gmail.com>
2022-03-08 18:47:41 +0600

    Working on webRTC connecting

--- src/views/pages/Calling.js
+++ src/views/pages/Calling.js
@@ -1,50 +1,52 @@
 import React, { useState }                                   from "react";
 import { StatusBar, StyleSheet, Text }                       from "react-native";
 import { CardItem, Container, Content, Input, Button, Body } from "native-base";
+import AsyncStorage                                          from "@react-native-async-storage/async-storage";
 
-const Calling = () => {
-  const [number, setNumber] = useState("");
+import {
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  mediaDevices,
+  registerGlobals,
+} from "react-native-webrtc";
 
-  const calling = () => {
-    console.log(number, "number");
-  };
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <Container>
-        <Content>
-          <CardItem header>
-            <Text style={styles.heading}>Calling</Text>
-          </CardItem>
-          <CardItem>
-            <Input value={number} onChangeText={(value) => setNumber(value)}
-                   placeholder="Enter Number for calling" style={styles.input} />
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Button primary block onPress={() => calling()}>
-                <Text style={styles.btn}>Calling</Text>
-              </Button>
-            </Body>
-          </CardItem>
-        </Content>
-      </Container>
-    </>
-  );
+export default function Calling({ navigation, ...props }) {
+  // const configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
+  // const pc = new RTCPeerConnection(configuration);
+
+  return (<>
+    <StatusBar barStyle="dark-content" />
+    <Container>
+      <Content>
+        <CardItem header>
+          <Text style={styles.heading}>Calling</Text>
+        </CardItem>
+        <CardItem>
+          <Input
+            placeholder="Enter Number for calling" style={styles.input} />
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Button mode="contained"
+                    primary block>
+              <Text style={styles.btn}>Calling</Text>
+            </Button>
+          </Body>
+        </CardItem>
+      </Content>
+    </Container>
+  </>);
 };
-const styles  = StyleSheet.create({
-  heading: {
-    textAlign: "center",
-    flex     : 1,
-    fontSize : 20,
-  },
-  input  : {
-    borderWidth: 1,
-    borderColor: "blue",
-  },
-  btn    : {
-    color   : "#fff",
-    fontSize: 22,
+const styles = StyleSheet.create({
+  heading : {
+    textAlign: "center", flex: 1, fontSize: 20,
+  }, input: {
+    borderWidth: 1, borderColor: "blue",
+  }, btn  : {
+    color: "#fff", fontSize: 22,
   },
 });
-export default Calling;
--- src/views/pages/ContactList.js
+++ src/views/pages/ContactList.js
@@ -1,14 +1,33 @@
-import React, { Component }                                 from "react";
-import { StatusBar, StyleSheet, Text, View }      from "react-native";
-import { Body, Button, CardItem, Container, Content, Item } from "native-base";
-import { PermissionsAndroid }                               from "react-native";
-import Contacts                                             from "react-native-contacts";
+import React, { Component }                                      from "react";
+import { Platform, StyleSheet, Text, View, FlatList, StatusBar } from "react-native";
+import { Body, Button, CardItem, Container, Content, Item }      from "native-base";
+import { PermissionsAndroid }                                    from "react-native";
+import Contacts                                                  from "react-native-contacts";
 
+
+const instructions = Platform.select({
+  ios    : "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu",
+});
 
 export default class ContactList extends Component {
   state = {
-    contacts: null,
+    contact: [],
   };
+
+  componentDidMount() {
+    //this.getContact();
+  }
+
+  getContact() {
+    Contacts.checkPermission((error, res) => {
+      if (res === "authorized") {
+        Contacts.getAll((err, contact) => this.setState({ contact }));
+      }
+    });
+  }
 
   getContacts() {
     if (Platform.OS === "ios") {
@@ -35,6 +54,17 @@ export default class ContactList extends Component {
     }
   }
 
+  renderItem({ item, index }) {
+    const number = item.phoneNumbers.map((val, key) => {
+      if (key === 0) return val.number;
+    }); //may be wrongly the save their name only without phonenumber so only i have show first element only
+    return (
+      <View style={{ flexDirection: "row", width: "100%", padding: 10, justifyContent: "space-between" }}>
+        <Text>{item.givenName} {item.middleName} {item.familyName}</Text>
+        <Text>{number}</Text>
+      </View>
+    );
+  }
 
   render() {
     const DATA = [
@@ -67,6 +97,12 @@ export default class ContactList extends Component {
             </Body>
           </CardItem>
           <CardItem>
+            <FlatList
+              style={{ width: "100%" }}
+              data={this.state.contact}
+              renderItem={(a) => this.renderItem(a)}
+              keyExtractor={(item, index) => index.toString()}
+            />
             {/*<FlatList*/}
             {/*  data={DATA}*/}
             {/*  renderItem={renderItem}*/}
